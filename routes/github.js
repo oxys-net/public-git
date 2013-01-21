0a756a5d1535fc3508101eed0f8c5030f6eae674
David Charbonnier <d.charbonnier@oxys.net>
2013-01-21 08:46:21 +0100

    more generic name

--- routes/github.js
+++ routes/github.js
@@ -1,15 +1,15 @@
 var Repository = require('../libs/github/Repository'),
     config = require('../config');
 
-var cafebabel = new Repository(config.get('GITHUB_USER'),config.get('GITHUB_PASSWORD'),config.get('GITHUB_REPOS'));
+var repository = new Repository(config.get('GITHUB_USER'),config.get('GITHUB_PASSWORD'),config.get('GITHUB_REPOS'));
 
 exports.issues = function(req, res){
   res.locals.page = "issues";
-  res.render('issues',cafebabel.templateData());
+  res.render('issues',repository.templateData());
 };
 
 
 exports.milestones = function(req, res){
   res.locals.page = "milestones";
-  res.render('milestones',cafebabel.templateData());
+  res.render('milestones',repository.templateData());
 };
